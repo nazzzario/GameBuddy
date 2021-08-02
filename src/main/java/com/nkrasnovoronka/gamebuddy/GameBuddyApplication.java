96074b072a2687c59c7c8b8db9f525aef3afb13a
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-08-02 19:30:42 +0300

    Initial commit

---
+++ src/main/java/com/nkrasnovoronka/gamebuddy/GameBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.nkrasnovoronka.gamebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nkrasnovoronka/gamebuddy/GameBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkrasnovoronka.gamebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
